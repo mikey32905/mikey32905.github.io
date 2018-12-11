c861828b48541b5e1733e9c3899843b1646b8744
Mike Williams <mikey32905@gmail.com>
2018-12-11 15:46:15 -0500

    Add files via upload
    did challenge 3 for hangman through Udemy class, updating the hangman app.

--- js-bootcamp/hangman/app.js
+++ js-bootcamp/hangman/app.js
@@ -1,6 +1,33 @@
+const hangmanGameElement = document.querySelector('#hangman-board')
+const puzzleBoardElement = document.querySelector('#puzzle-display')
+const lettersGuessedElement = document.querySelector('#letters-guessed')
+const guessesLeftElement = document.querySelector('#guesses-left')
 
-const product = 'Duh!!!!'
-console.log(product.split(''))
+const game1 = new Hangman()
+game1.setWord('chocolate')
+console.log(game1.status)
 
-const otherProduct = new String('Phone')
-console.log(otherProduct.split(''))
+window.addEventListener('keypress', function(e) {
+    const guess = String.fromCharCode(e.charCode)
+    game1.makeGuesses(guess)
+    this.console.log(game1.status)
+    puzzleBoardElement.textContent = game1.getPuzzle()
+    lettersGuessedElement.textContent = `Letters Guessed: ${game1.lettersGuessed}`
+    guessesLeftElement.textContent = `Number of guesses left: ${game1.guesses}`
+})
+
+
+hangmanGameElement.innerHTML = ''
+
+puzzleBoardElement.innerHTML = '<h3>'
+puzzleBoardElement.textContent = game1.getPuzzle()
+hangmanGameElement.appendChild(puzzleBoardElement)
+
+lettersGuessedElement.innerHTML = '<p>'
+lettersGuessedElement.textContent = `Letters Guessed: ${game1.lettersGuessed}`
+hangmanGameElement.appendChild(lettersGuessedElement)
+
+guessesLeftElement.innerHTML = '<p>'
+guessesLeftElement.textContent = `Number of guesses left: ${game1.guesses}`
+hangmanGameElement.appendChild(guessesLeftElement)
+
--- js-bootcamp/hangman/hangman.js
+++ js-bootcamp/hangman/hangman.js
@@ -1,7 +1,10 @@
-const Hangman = function(word = [], guesses, lettersGuessed = []) {
+
+//const words = ['chocolate', 'Merry Christmas', 'Spiderman', 'New Years Eve', 'winter', 'Titanic']
+const Hangman = function(word = [], guesses, lettersGuessed = [], status = 'playing') {
     this.word = word
     this.guesses = guesses
     this.lettersGuessed = lettersGuessed
+    this.status = status
 }
 
 Hangman.prototype.setWord = function(word) {
@@ -22,16 +25,11 @@ Hangman.prototype.makeGuesses = function(guess) {
     if (isUnique && isBadGuess) {
         this.guesses--
     }
+
+    this.calculateStatus()
  }
 
-
 Hangman.prototype.getPuzzle = function() {
-    const puzzleResult = {
-        puzzle: '',
-        lettersGuessed: '',
-        guessesLeft: 0
-    }
-
     let puzzle = ''
 
     this.word.forEach((letter) => {
@@ -40,60 +38,18 @@ Hangman.prototype.getPuzzle = function() {
         } else {
             puzzle += '*'
         }
-
     })
 
-    this.lettersGuessed.forEach((letter) => {
-        puzzleResult.lettersGuessed += letter + ' '
-    })
-
-    puzzleResult.puzzle = puzzle
-    puzzleResult.guessesLeft = this.guesses
-
-    return `Your puzzle results: \r\n ${puzzleResult.puzzle}   You have guessed: ${puzzleResult.lettersGuessed}.  You have ${puzzleResult.guessesLeft} remaining.`
+    return puzzle
 }
 
-const game1 = new Hangman()
-game1.setWord('chocolate')
-console.log(game1.getPuzzle())
-this.console.log(game1.guesses)
+Hangman.prototype.calculateStatus = function() {
+    if (this.guesses === 0) {
+        this.status = 'failed'
+    } else if (!this.getPuzzle().includes('*')) {
+        this.status = 'finished'
+    } else {
+        this.status = 'playing'
+    }
+}
 
-window.addEventListener('keypress', function(e) {
-    const guess = String.fromCharCode(e.charCode)
-    //this.console.log(guess)
-    game1.makeGuesses(guess)
-    console.log(game1.getPuzzle())
-    this.console.log(game1.guesses)
-})
-
-
-/* game1.makeGuesses('O')
-game1.makeGuesses('c')
-game1.makeGuesses('g')
-game1.makeGuesses('N') */
-//game1.makeGuesses(['o','c','g','n'])
-/* const game2 = new Hangman()
-game2.setWord('Christmas')
-game2.makeGuesses('H')
-game2.makeGuesses('s')
-game2.makeGuesses('z')
-//game2.makeGuesses(['h','s','z','g'])
-const game3 = new Hangman()
-game3.setWord('Merry Christmas')
-game3.makeGuesses('M')
-game3.makeGuesses('t')
-game3.makeGuesses('r')
-game3.makeGuesses('z')
-game3.makeGuesses('q')
-game3.makeGuesses('C')
-//game3.makeGuesses(['m','t','r','z','q'])
-
-console.log(game1)
-console.log(game1.word.length)
-console.log(game1.getPuzzle())
-console.log(game2)
-console.log(game2.getPuzzle())
-console.log(game3)
-console.log(game3.getPuzzle())
- */
-//console.log([].length)
