7978b1fd77b59b6cfaa14aabf9553874d1188634
Mike Williams <mikey32905@gmail.com>
2018-12-17 15:47:15 -0500

    Add files via upload

--- js-bootcamp/functions/promises.js
+++ js-bootcamp/functions/promises.js
@@ -1,32 +1,62 @@
-const getDataCallBack = (callback) => {
+const getDataCallBack = (num, callback) => {
     setTimeout(() => {
-       callback(undefined,'This time is up!')
+        if (typeof num === 'number') {
+            callback(undefined, num * 2)
+        } else {
+            callback('Number must be provided!')
+        }
     }, 2000)
 }
 
-getDataCallBack((err,data) => {
+getDataCallBack(2,(err,data) => {
     if (err) {
         console.log(err)
     } else {
-        console.log(data)
+        getDataCallBack(data, (err, data) => {
+            if (err) {
+                console.log('err')
+            } else {
+                console.log(data)
+            }
+        })
     }
 })
 
 //Promise
 
-const getDataPromise = (data) =>  new Promise((resolve, reject) => {
+const getDataPromise = (num) =>  new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(`This is my success data: ${data}!!`)
-          // reject('This is my promise error')
+           typeof num === 'number' ? resolve(num * 2) : reject ('Number must be provided')
         }, 2000)
     }) 
 
 
-const myPromise = getDataPromise(123)
+/* const myPromise = getDataPromise(123)
 
 myPromise.then((data) => {
     console.log(data)
 
 }, (err) => {
+    console.log(err)
+}) */
+
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log(data)
+    }, (err) => {
+        console.log(err)
+    })
+}, (err) => {
+    console.log(err)
+})
+
+//Promise chaining
+getDataPromise(10).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    console.log(data)
+}).catch((err) => {
     console.log(err)
 })
