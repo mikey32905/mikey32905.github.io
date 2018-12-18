cd440895339ad23f60ba1b33153f117a5db594f5
Mike Williams <mikey32905@gmail.com>
2018-12-18 15:47:27 -0500

    Add files via upload
    completed first running battleship app.

--- js-bootcamp/battleship/battleship.js
+++ js-bootcamp/battleship/battleship.js
@@ -1,43 +1,50 @@
 //let board = []
-const battleshipBoardElement = document.querySelector('#puzzle-display')
+const battleshipBoardElement = document.querySelector('#battleship-board')
+const puzzleDisplayElement = document.querySelector('#puzzle-display')
 const titleElement = document.querySelector('#title')
 const guessRowElement = document.querySelector('#row-guess')
 const guessColumnElement = document.querySelector('#column-guess')
-const fireButtonElement = document.querySelector('#fire')
+const fireButtonElement = document.querySelector('#fire-button')
+const gameStatusElement = document.querySelector('#guesses-left')
 //const lf = document.createElement('br')
 
+//firebutton not working
 let gameBoard = []
-let turn = 1
+//let turn = 1
+const gameStatus = {
+    turn: 1,
+    status: 'starting'
+}
 
-titleElement.textContent = "Let's play Battleship!!"
 const generateGameBoard = (board) => {
-    battleshipBoardElement.innerHTML = ''
-    
+    puzzleDisplayElement.innerHTML = ''
+    console.log(`Game Status: ${gameStatus.status}`)
     for (let i = 0; i < 9; i++) {
         //create an empty line
-        board.push([])
-        //add columns to empty line
-        board[i].push(new Array(9))
+        if (gameStatus.status === 'starting') {
+            board.push([])
+        
+            //add columns to empty line
+            board[i].push(new Array(9))
+        }
 
         for (let j = 0; j < 9; j++) {
-            board[i][j] = "O"
+            if (board[i][j] !== "X" && board[i][j] !== "*") {
+                board[i][j] = "O"
+            }
         }
     }
 
     gameBoard = board
 
      board.forEach((row) => {
-        battleshipBoardElement.innerHTML += (row + "<br />") 
+        puzzleDisplayElement.innerHTML += (row + "<br />") 
     }) 
 }
 
-const getGuessRow = () => {
-    return parseInt(guessRowElement.value)
-}
+let getGuessRow = NaN
 
-const getGuessColumn = () => {
-    return parseInt(guessColumnElement.value)
-}
+let getGuessColumn = NaN
 
 const randomRow = () => {
     return Math.floor(Math.random() * 9)
@@ -47,40 +54,84 @@ const randomColumn = () => {
     return Math.floor(Math.random() * 9)   
 }
 
+const generateStatusMessage = () => {
+    if (gameStatus.status === 'playing') {
+        gameStatusElement.textContent = `You missed!  Try again! Turn: ${gameStatus.turn}`
+    } else if (gameStatus.status === 'finished') {
+        gameStatusElement.textContent = 'Congratulations! You sunk my battleship!'
+    } else if (gameStatus.status === 'failed') {
+        gameStatusElement.textContent = `Game Over!! Ship was located at: ${shipRow}, ${shipColumn}. `
+    } else {
+        gameStatusElement.textContent = 'Good luck!! You may start your game!'
+    }
+}
+
+
 generateGameBoard(gameBoard)
 const shipRow = randomRow()
 const shipColumn = randomColumn()
 
 console.log(`Ship's Row: ${shipRow}  Ship's Column: ${shipColumn}`)
+titleElement.textContent = "Let's play Battleship!!"
+generateStatusMessage()
+gameStatus.status = 'playing'
 
-
-
-fireButtonElement.addEventListener('click', () => {
-    const guessRow = getGuessRow()
-    const guessColumn = getGuessColumn()
-    console.log(`Guess Row: ${guessRow}  Guess Column: ${guessColumn}`)
-   // debugger
-    if (guessRow === shipRow && guessColumn === shipColumn) {
-        console.log('Congratulations! You sunk my battleship!')
-        gameBoard[shipRow][shipColumn] = '*'
-    } else {
-        if (guessRow < 0 || guessRow > 4) {
-            console.log('Oops, that\'s not even in the ocean!')
-        } else if (gameBoard[guessRow][guessColumn] === 'X') {
-            console.log('You guessed that one already!!')
+guessRowElement.addEventListener('input', (e) => {
+    console.log(`Row Guess: ${e.target.value}`)
+    if (gameStatus.status === 'playing') {
+        if (isNaN(e.target.value)) {
+            console.log('Row guess must be number 0 - 8')
         } else {
-            console.log('You missed my battleship!')
+            getGuessRow = parseInt(e.target.value)
         }
-
-        if (turn === 9) {
-            console.log('Game Over!!')
-            gameBoard[shipRow][shipColumn] = "*"
-            console.log(`Ship located at: ${shipRow}, ${shipColumn}.`)
-        }
-
-        gameBoard[guessRow][guessColumn] = 'X'
-        turn++
-        console.log(`Turn: ${turn}`)
-        generateGameBoard(gameBoard)
     }
 })
+
+guessColumnElement.addEventListener('input', (e) => {
+    console.log(`Column Guess: ${e.target.value}`)
+    if (gameStatus.status === 'playing') {
+        if (isNaN(e.target.value)) {
+            console.log('Column guess must be number 0 - 8')
+        } else {
+            getGuessColumn = parseInt(e.target.value)
+        }
+    }
+})
+
+fireButtonElement.addEventListener('click', (e) => {
+    if (gameStatus.status === 'playing') {
+        console.log(`Guess Row: ${getGuessRow}  Guess Column: ${getGuessColumn}`)
+        // debugger
+            if (getGuessRow === shipRow && getGuessColumn === shipColumn) {
+                gameStatus.status = 'finished'
+                gameBoard[shipRow][shipColumn] = '*'
+            } else {
+                if (getGuessRow < 0 || getGuessRow > 4) {
+                    console.log('Oops, that\'s not even in the ocean!')
+                } else if (gameBoard[getGuessRow][getGuessColumn] === 'X') {
+                    console.log('You guessed that one already!!')
+                } else {
+                    console.log('You missed my battleship!')
+                }
+
+                if (gameStatus.turn === 9) {
+        //            console.log('Game Over!!')
+                    gameStatus.status = 'failed'
+                    gameBoard[shipRow][shipColumn] = "*"
+        //            console.log(`Ship located at: ${shipRow}, ${shipColumn}.`)
+                } else {
+                    gameStatus.status = 'playing'
+                    gameBoard[getGuessRow][getGuessColumn] = 'X'
+                    gameStatus.turn++
+                }
+
+            }
+
+            generateStatusMessage()
+            generateGameBoard(gameBoard)
+            guessRowElement.value = null
+            guessColumnElement.value = null
+    }
+})
+
+
