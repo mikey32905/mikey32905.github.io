affef293837d19c725ea5fcd5a055adc1c5e5f0f
Mike Williams <mikey32905@gmail.com>
2018-12-20 14:49:39 -0500

    update urls

--- js-bootcamp/hangman/scripts/requests.js
+++ js-bootcamp/hangman/scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzle = async(wordCount) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
  
     if (response.status === 200) {
         const data = await response.json()
@@ -16,7 +16,7 @@ const getCurrentCountry = async() => {
 }
 
 const getCountry = async (countryCode) => {
-    const response = await fetch('http://restcountries.eu/rest/v2/all')
+    const response = await fetch('//restcountries.eu/rest/v2/all')
 
     if (response.status === 200) {
         const data = await response.json()
@@ -27,7 +27,7 @@ const getCountry = async (countryCode) => {
 }
 
 const getLocation = async () => {
-    const response = await fetch('http://ipinfo.io/json?token=30ebdecc219207')
+    const response = await fetch('//ipinfo.io/json?token=30ebdecc219207')
  
     if (response.status === 200) {
        return  response.json()
